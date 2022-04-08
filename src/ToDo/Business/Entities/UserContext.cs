132a6d2769525b6cc68ab7776b880f7422d4861a
Carlín Yahuira Achahui <43356162+ClayAchahui@users.noreply.github.com>
2022-04-08 18:14:19 -0500

    Update UserContext.cs

--- src/ToDo/Business/Entities/UserContext.cs
+++ src/ToDo/Business/Entities/UserContext.cs
@@ -6,18 +6,7 @@ namespace ToDo.Business.Entities
 {
 	public class UserContext
 	{
-		public string? aud { get; set; }
-		public string? iss { get; set; }
-		public int iat { get; set; }
-		public int nbf { get; set; }
-		public int exp { get; set; }
 		public string? name { get; set; }
-		public string? oid { get; set; }
 		public string? preferred_username { get; set; }
-		public string? rh { get; set; }
-		public string? sub { get; set; }
-		public string? tid { get; set; }
-		public string? uti { get; set; }
-		public string? ver { get; set; }
 	}
 }
