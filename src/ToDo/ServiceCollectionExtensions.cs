dd71c89fe98b0214a9310f0e25fb96ba5179014c
Nick Randolph <nick@builttoroam.com>
2022-04-12 16:05:03 +1000

    fix: Updating to latest uno extensions

--- src/ToDo/ServiceCollectionExtensions.cs
+++ src/ToDo/ServiceCollectionExtensions.cs
@@ -7,9 +7,13 @@ public static class ServiceCollectionExtensions
 	public static IServiceCollection AddEndpoints(
 		this IServiceCollection services,
 		HostBuilderContext context,
-		Func<Task<string>> accessTokenCallback)
+		Func<IServiceProvider, Task<string>> accessTokenCallback)
 	{
-		Action<RefitSettings> authSettingsBuilder = settings => settings.AuthorizationHeaderValueGetter = accessTokenCallback;
+		Action<IServiceProvider, RefitSettings> authSettingsBuilder = (sp, settings) =>
+		{
+			settings.AuthorizationHeaderValueGetter = () => accessTokenCallback(sp);
+		};
+
 		return services
 			.AddNativeHandler()
 			.AddContentSerializer()
@@ -21,140 +25,140 @@ public static class ServiceCollectionExtensions
 		=> services
 			.AddSingleton<ITaskService, TaskService>()
 			.AddSingleton<ITaskListService, TaskListService>()
-			.AddSingleton< CommunityToolkit.Mvvm.Messaging.IMessenger, CommunityToolkit.Mvvm.Messaging.WeakReferenceMessenger>();
+			.AddSingleton<CommunityToolkit.Mvvm.Messaging.IMessenger, CommunityToolkit.Mvvm.Messaging.WeakReferenceMessenger>();
 
 
 	/*********************** Temporary extensions until Uno.Extensions.Http is updated **************/
 	// See: https://github.com/unoplatform/uno.extensions/pull/347
 
-	private static char[] InterfaceNamePrefix = new[] { 'i', 'I' };
+	//private static char[] InterfaceNamePrefix = new[] { 'i', 'I' };
 
-	public static IServiceCollection AddClient<TClient, TImplementation>(
-			this IServiceCollection services,
-			HostBuilderContext context,
-			string? name = null,
-			Func<IHttpClientBuilder, EndpointOptions, IHttpClientBuilder>? configure = null
-		)
-			where TClient : class
-			where TImplementation : class, TClient
-	{
-		Func<IServiceCollection, HostBuilderContext, IHttpClientBuilder> httpClientFactory = (s, c) => s.AddHttpClient<TClient, TImplementation>();
+	//public static IServiceCollection AddClient<TClient, TImplementation>(
+	//		this IServiceCollection services,
+	//		HostBuilderContext context,
+	//		string? name = null,
+	//		Func<IHttpClientBuilder, EndpointOptions, IHttpClientBuilder>? configure = null
+	//	)
+	//		where TClient : class
+	//		where TImplementation : class, TClient
+	//{
+	//	Func<IServiceCollection, HostBuilderContext, IHttpClientBuilder> httpClientFactory = (s, c) => s.AddHttpClient<TClient, TImplementation>();
 
-		return services.AddClient<TClient>(context, name, httpClientFactory, configure);
-	}
+	//	return services.AddClient<TClient>(context, name, httpClientFactory, configure);
+	//}
 
-	public static IServiceCollection AddClient<TClient, TImplementation>(
-		this IServiceCollection services,
-		HostBuilderContext context,
-		EndpointOptions options,
-		string? name = null,
-		Func<IHttpClientBuilder, EndpointOptions, IHttpClientBuilder>? configure = null
-	)
-		where TClient : class
-		where TImplementation : class, TClient
-	{
-		Func<IServiceCollection, HostBuilderContext, IHttpClientBuilder> httpClientFactory = (s, c) => s.AddHttpClient<TClient, TImplementation>();
+	//public static IServiceCollection AddClient<TClient, TImplementation>(
+	//	this IServiceCollection services,
+	//	HostBuilderContext context,
+	//	EndpointOptions options,
+	//	string? name = null,
+	//	Func<IHttpClientBuilder, EndpointOptions, IHttpClientBuilder>? configure = null
+	//)
+	//	where TClient : class
+	//	where TImplementation : class, TClient
+	//{
+	//	Func<IServiceCollection, HostBuilderContext, IHttpClientBuilder> httpClientFactory = (s, c) => s.AddHttpClient<TClient, TImplementation>();
 
-		return services.AddClient<TClient>(context, options, name, httpClientFactory, configure);
-	}
+	//	return services.AddClient<TClient>(context, options, name, httpClientFactory, configure);
+	//}
 
-	public static IServiceCollection AddClient<TInterface>(
-		this IServiceCollection services,
-		HostBuilderContext context,
-		string? name = null,
-		Func<IServiceCollection, HostBuilderContext, IHttpClientBuilder>? httpClientFactory = null,
-		Func<IHttpClientBuilder, EndpointOptions, IHttpClientBuilder>? configure = null
-	)
-		where TInterface : class
-	{
-		name ??= typeof(TInterface).IsInterface ? typeof(TInterface).Name.TrimStart(InterfaceNamePrefix) : typeof(TInterface).Name;
-		var options = Microsoft.Extensions.Configuration.ConfigurationBinder.Get<EndpointOptions>(context.Configuration.GetSection(name));//.Get<EndpointOptions>();
-		return services.AddClient<TInterface>(context, options!, name, httpClientFactory, configure);
-	}
+	//public static IServiceCollection AddClient<TInterface>(
+	//	this IServiceCollection services,
+	//	HostBuilderContext context,
+	//	string? name = null,
+	//	Func<IServiceCollection, HostBuilderContext, IHttpClientBuilder>? httpClientFactory = null,
+	//	Func<IHttpClientBuilder, EndpointOptions, IHttpClientBuilder>? configure = null
+	//)
+	//	where TInterface : class
+	//{
+	//	name ??= typeof(TInterface).IsInterface ? typeof(TInterface).Name.TrimStart(InterfaceNamePrefix) : typeof(TInterface).Name;
+	//	var options = Microsoft.Extensions.Configuration.ConfigurationBinder.Get<EndpointOptions>(context.Configuration.GetSection(name));//.Get<EndpointOptions>();
+	//	return services.AddClient<TInterface>(context, options!, name, httpClientFactory, configure);
+	//}
 
-	public static IServiceCollection AddClient<TInterface>(
-		this IServiceCollection services,
-		HostBuilderContext context,
-		EndpointOptions options,
-		string? name = null,
-		Func<IServiceCollection, HostBuilderContext, IHttpClientBuilder>? httpClientFactory = null,
-		Func<IHttpClientBuilder, EndpointOptions, IHttpClientBuilder>? configure = null
-	)
-		where TInterface : class
-	{
-		name ??= typeof(TInterface).IsInterface ? typeof(TInterface).Name.TrimStart(InterfaceNamePrefix) : typeof(TInterface).Name;
+	//public static IServiceCollection AddClient<TInterface>(
+	//	this IServiceCollection services,
+	//	HostBuilderContext context,
+	//	EndpointOptions options,
+	//	string? name = null,
+	//	Func<IServiceCollection, HostBuilderContext, IHttpClientBuilder>? httpClientFactory = null,
+	//	Func<IHttpClientBuilder, EndpointOptions, IHttpClientBuilder>? configure = null
+	//)
+	//	where TInterface : class
+	//{
+	//	name ??= typeof(TInterface).IsInterface ? typeof(TInterface).Name.TrimStart(InterfaceNamePrefix) : typeof(TInterface).Name;
 
-		if (httpClientFactory is null)
-		{
-			httpClientFactory = (s, c) => s.AddHttpClient(name);
-		}
+	//	if (httpClientFactory is null)
+	//	{
+	//		httpClientFactory = (s, c) => s.AddHttpClient(name);
+	//	}
 
-		var httpClientBuilder = httpClientFactory(services, context);
+	//	var httpClientBuilder = httpClientFactory(services, context);
 
-		_ = httpClientBuilder
-			.Conditional(
-				options.UseNativeHandler,
-				builder => builder.ConfigurePrimaryAndInnerHttpMessageHandler<HttpMessageHandler>())
-			.ConfigureHttpClient((serviceProvider, client) =>
-			{
-				if (options.Url is not null)
-				{
-					client.BaseAddress = new Uri(options.Url);
-				}
-			})
-			.Conditional(
-				configure is not null,
-				builder => configure?.Invoke(builder, options) ?? builder);
-		return services;
-	}
-
-
-	public static IHttpClientBuilder ConfigurePrimaryAndInnerHttpMessageHandler<THandler>(this IHttpClientBuilder builder) where THandler : HttpMessageHandler
-	{
-		if (builder == null)
-		{
-			throw new ArgumentNullException("builder");
-		}
-
-		builder.Services.Configure(builder.Name, delegate (HttpClientFactoryOptions options)
-		{
-			options.HttpMessageHandlerBuilderActions.Add(delegate (HttpMessageHandlerBuilder b)
-			{
-				var innerHandler = b.Services.GetRequiredService<THandler>() as HttpMessageHandler;
-				if (b.PrimaryHandler is DelegatingHandler delegatingHandler)
-				{
-					delegatingHandler.InnerHandler = innerHandler;
-					innerHandler = delegatingHandler;
-				}
-
-				b.PrimaryHandler = innerHandler;
-			});
-		});
-		return builder;
-	}
+	//	_ = httpClientBuilder
+	//		.Conditional(
+	//			options.UseNativeHandler,
+	//			builder => builder.ConfigurePrimaryAndInnerHttpMessageHandler<HttpMessageHandler>())
+	//		.ConfigureHttpClient((serviceProvider, client) =>
+	//		{
+	//			if (options.Url is not null)
+	//			{
+	//				client.BaseAddress = new Uri(options.Url);
+	//			}
+	//		})
+	//		.Conditional(
+	//			configure is not null,
+	//			builder => configure?.Invoke(builder, options) ?? builder);
+	//	return services;
+	//}
 
 
-	public static IServiceCollection AddRefitClient<TInterface>(
-		this IServiceCollection services,
-		HostBuilderContext context,
-		string? name = null,
-		Action<RefitSettings>? settingsBuilder = null,
-		Func<IHttpClientBuilder, EndpointOptions, IHttpClientBuilder>? configure = null
-	)
-		where TInterface : class
-	{
-		return services.AddClient<TInterface>(
-			context,
-			name,
-			(s, c) => Refit.HttpClientFactoryExtensions.AddRefitClient<TInterface>(s, settingsAction: serviceProvider =>
-			{
-				var settings = new RefitSettings()
-				{
-					ContentSerializer = serviceProvider.GetRequiredService<IHttpContentSerializer>(),
-				};
-				settingsBuilder?.Invoke(settings);
-				return settings;
-			}),
-			configure);
-	}
+	//public static IHttpClientBuilder ConfigurePrimaryAndInnerHttpMessageHandler<THandler>(this IHttpClientBuilder builder) where THandler : HttpMessageHandler
+	//{
+	//	if (builder == null)
+	//	{
+	//		throw new ArgumentNullException("builder");
+	//	}
+
+	//	builder.Services.Configure(builder.Name, delegate (HttpClientFactoryOptions options)
+	//	{
+	//		options.HttpMessageHandlerBuilderActions.Add(delegate (HttpMessageHandlerBuilder b)
+	//		{
+	//			var innerHandler = b.Services.GetRequiredService<THandler>() as HttpMessageHandler;
+	//			if (b.PrimaryHandler is DelegatingHandler delegatingHandler)
+	//			{
+	//				delegatingHandler.InnerHandler = innerHandler;
+	//				innerHandler = delegatingHandler;
+	//			}
+
+	//			b.PrimaryHandler = innerHandler;
+	//		});
+	//	});
+	//	return builder;
+	//}
+
+
+	//public static IServiceCollection AddRefitClient<TInterface>(
+	//	this IServiceCollection services,
+	//	HostBuilderContext context,
+	//	string? name = null,
+	//	Action<RefitSettings>? settingsBuilder = null,
+	//	Func<IHttpClientBuilder, EndpointOptions, IHttpClientBuilder>? configure = null
+	//)
+	//	where TInterface : class
+	//{
+	//	return services.AddClient<TInterface>(
+	//		context,
+	//		name,
+	//		(s, c) => Refit.HttpClientFactoryExtensions.AddRefitClient<TInterface>(s, settingsAction: serviceProvider =>
+	//		{
+	//			var settings = new RefitSettings()
+	//			{
+	//				ContentSerializer = serviceProvider.GetRequiredService<IHttpContentSerializer>(),
+	//			};
+	//			settingsBuilder?.Invoke(settings);
+	//			return settings;
+	//		}),
+	//		configure);
+	//}
 }
--- src/ToDo/ToDoSettings.cs
+++ src/ToDo/ToDoSettings.cs
@@ -4,4 +4,7 @@ namespace ToDo;
 public record ToDoSettings
 {
 	public string? LastSearch { get; init; }
+
+	public string? CachedAccessToken { get; init; }
+	public DateTime? CachedAccessTokenTimeStamp { get; init; }
 }
