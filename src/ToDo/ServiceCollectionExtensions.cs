8fd615eba9e7c23b60e5496eda0f3d5534c3dc61
Nick Randolph <nick@builttoroam.com>
2022-04-21 18:50:50 +1000

    fix: Fixing build error with changes to auth flow

--- src/ToDo/ServiceCollectionExtensions.cs
+++ src/ToDo/ServiceCollectionExtensions.cs
@@ -1,6 +1,6 @@
 ﻿// Only define mocks in debug as we don't have a way to dynamically switch them
 #if DEBUG
-//#define USE_MOCKS
+#define USE_MOCKS
 #endif
 
 using ToDo.Business.Services;
@@ -11,35 +11,34 @@ public static class ServiceCollectionExtensions
 {
 	public static IServiceCollection AddEndpoints(
 		this IServiceCollection services,
-		HostBuilderContext context)
+		HostBuilderContext context,
+		Action<IServiceProvider, RefitSettings>? settingsBuilder = null)
 	{
 		return services
 			.AddNativeHandler()
 			.AddContentSerializer()
-			.AddRefitClient<ITaskEndpoint>(context, nameof(ITaskEndpoint))
-			.AddRefitClient<ITaskListEndpoint>(context, nameof(ITaskEndpoint))
+			.AddRefitClient<ITaskEndpoint>(context, nameof(ITaskEndpoint), settingsBuilder)
+			.AddRefitClient<ITaskListEndpoint>(context, nameof(ITaskEndpoint), settingsBuilder)
 
 // Comment out the USE_MOCKS definition (top of this file) to prevent using mocks in development
 #if USE_MOCKS
-						.AddSingleton<ITaskListEndpoint, ToDo.Data.Mock.MockTaskListEndpoint>()
-						.AddSingleton<ITaskEndpoint, ToDo.Data.Mock.MockTaskEndpoint>()
+			.AddSingleton<ITaskListEndpoint, ToDo.Data.Mock.MockTaskListEndpoint>()
+			.AddSingleton<ITaskEndpoint, ToDo.Data.Mock.MockTaskEndpoint>()
 #endif
-						;
+			;
 	}
 
 	public static IServiceCollection AddServices(this IServiceCollection services)
 	=> services
 		.AddSingleton<ITaskService, TaskService>()
 		.AddSingleton<ITaskListService, TaskListService>()
-					.AddSingleton<IAuthenticationService, AuthenticationService>()
-					.AddSingleton<IAuthenticationTokenProvider>(sp => sp.GetRequiredService<IAuthenticationService>())
+		.AddSingleton<IAuthenticationService, AuthenticationService>()
+		.AddSingleton<IAuthenticationTokenProvider>(sp => sp.GetRequiredService<IAuthenticationService>())
 		.AddSingleton<IMessenger, WeakReferenceMessenger>()
 
-// Comment out the USE_MOCKS definition (top of this file) to prevent using mocks in development
+		// Comment out the USE_MOCKS definition (top of this file) to prevent using mocks in development
 #if USE_MOCKS
-						.AddSingleton<IAuthenticationService, MockAuthenticationService>()
+		.AddSingleton<IAuthenticationService, MockAuthenticationService>()
 #endif
-
-
 		;
 }
