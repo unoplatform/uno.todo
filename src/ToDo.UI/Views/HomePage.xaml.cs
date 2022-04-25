7759e901b6b6e439760d05f5b45b73dc1d064e09
xiaoy312 <xiaoyao312@gmail.com>
2022-04-25 17:02:45 -0400

    feat: nav-view with fullscreen pane on mobile

--- src/ToDo.UI/Views/HomePage.xaml.cs
+++ src/ToDo.UI/Views/HomePage.xaml.cs
@@ -3,14 +3,14 @@ namespace ToDo.Views;
 
 public sealed partial class HomePage : Page
 {
-    public HomePage()
-    {
-        this.InitializeComponent();
-    }
+	public HomePage()
+	{
+		this.InitializeComponent();
+		this.Loaded += (s, e) => NavView.IsPaneOpen = true;
+	}
 
 	public async void CreateTaskListClick(object sender, RoutedEventArgs args)
 	{
-
 		var response = await Navigator!.NavigateViewModelForResultAsync<AddListViewModel, TaskListRequestData>(this, qualifier: Qualifiers.Dialog);
 		if (response is null)
 		{
@@ -23,6 +23,11 @@ public sealed partial class HomePage : Page
 
 	}
 
+	private void OpenNavPane(object sender, RoutedEventArgs e)
+	{
+		NavView.IsPaneOpen = true;
+	}
+
 	private INavigator? Navigator { get; set; }
-	public void Inject(INavigator entity) => Navigator=entity;
+	public void Inject(INavigator entity) => Navigator = entity;
 }
