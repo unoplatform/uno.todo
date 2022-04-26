4dde2f2d990a64fc95672ec9ae0aaef357e781b5
Carlín Yahuira Achahui <43356162+ClayAchahui@users.noreply.github.com>
2022-04-26 09:39:52 -0500

    mark as completed and important

--- src/ToDo/Presentation/TaskNoteViewModel.cs
+++ src/ToDo/Presentation/TaskNoteViewModel.cs
@@ -2,5 +2,23 @@ namespace ToDo.Presentation;
 
 public partial class TaskNoteViewModel
 {
-    
+	private INavigator Navigator { get; }
+
+	public ICommand AddNoteCommand { get; }
+
+	public string? Note { get; set; }
+
+	public TaskNoteViewModel(
+		INavigator navigator)
+	{
+
+		Navigator = navigator;
+
+		AddNoteCommand = new AsyncRelayCommand(AddNote);
+	}
+
+	public async Task AddNote()
+	{
+		await Navigator.NavigateBackWithResultAsync(this, data: new TaskBodyData { Content = Note });
+	}
 }
