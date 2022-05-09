b281bf667117c4a0a7072a41899967db8287fafa
Jerome Laban <jerome.laban@nventive.com>
2022-05-09 16:43:02 -0400

    ci: Store browser logs

--- src/ToDo.UI.Tests/TestBase.cs
+++ src/ToDo.UI.Tests/TestBase.cs
@@ -1,17 +1,22 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using System;
 using System.IO;
 using Uno.UITest;
+using Uno.UITest.Helpers.Queries;
 using Uno.UITest.Selenium;
 using Uno.UITests.Helpers;
 
 namespace ToDo.UI.Tests
 {
     public class TestBase
-    {
-        private IApp? _app;
+	{
+		private readonly string _screenShotPath = Environment.GetEnvironmentVariable("UNO_UITEST_SCREENSHOT_PATH");
 
-        static TestBase()
+		private IApp? _app;
+		private DateTime _startTime;
+
+		static TestBase()
         {
             AppInitializer.TestEnvironment.AndroidAppName = Constants.AndroidAppName;
             AppInitializer.TestEnvironment.WebAssemblyDefaultUri = Constants.WebAssemblyDefaultUri;
@@ -43,16 +48,50 @@ namespace ToDo.UI.Tests
         [SetUp]
         public void SetUpTest()
         {
-            App = AppInitializer.AttachToApp();
+			_startTime = DateTime.Now;
+			App = AppInitializer.AttachToApp();
         }
 
-        [TearDown]
-        public void TearDownTest()
-        {
-            TakeScreenshot("teardown");
-        }
+		[TearDown]
+		public void AfterEachTest()
+		{
+			if (
+				TestContext.CurrentContext.Result.Outcome != ResultState.Success
+				&& TestContext.CurrentContext.Result.Outcome != ResultState.Skipped
+				&& TestContext.CurrentContext.Result.Outcome != ResultState.Ignored
+			)
+			{
+				TakeScreenshot($"{TestContext.CurrentContext.Test.Name} - Tear down on error");
+			}
 
-        public FileInfo TakeScreenshot(string stepName)
+			WriteSystemLogs(GetCurrentStepTitle("log"));
+		}
+
+		private void WriteSystemLogs(string fileName)
+		{
+			if (_app != null && AppInitializer.GetLocalPlatform() == Platform.Browser)
+			{
+				var outputPath = string.IsNullOrEmpty(_screenShotPath)
+					? Environment.CurrentDirectory
+					: _screenShotPath;
+
+				using (var logOutput = new StreamWriter(Path.Combine(outputPath, $"{fileName}_{DateTime.Now:yyyy-MM-dd-HH-mm-ss.fff}.txt")))
+				{
+					foreach (var log in _app.GetSystemLogs(_startTime.ToUniversalTime()))
+					{
+						logOutput.WriteLine($"{log.Timestamp}/{log.Level}: {log.Message}");
+					}
+				}
+			}
+		}
+
+		private static string GetCurrentStepTitle(string stepName) =>
+					$"{TestContext.CurrentContext.Test.Name}_{stepName}"
+						.Replace(" ", "_")
+						.Replace(".", "_")
+						.Replace("__", "_");
+
+		public FileInfo TakeScreenshot(string stepName)
         {
             if(_app == null)
             {
