ba0d8bdd92c3b1466a6b6dfbbd3bc95c5c9fdcc9
Jerome Laban <jerome.laban@nventive.com>
2022-05-09 16:24:26 -0400

    ci: Adjust lookups

--- src/ToDo.UI.Tests/Given_MainPage.cs
+++ src/ToDo.UI.Tests/Given_MainPage.cs
@@ -8,30 +8,28 @@ namespace ToDo.UI.Tests
         [Test]
         public void When_SmokeTest()
         {
-			// Make sure the ViewModelButton has rendered
-			App.WaitForElement(q => q.Marked("WelcomePage_Button_Wide"));
-
-			// Query for the XamlButton and then tap it
 			Query WelcomePage_GetStarted = q => q.All().Marked("WelcomePage_Button_Wide");
-			App.WaitForElement(WelcomePage_GetStarted);
+
+			// Make sure the ViewModelButton has rendered
+			App.WaitForElement(WelcomePage_GetStarted, timeoutMessage: "Timeout waiting for WelcomePage_Button_Wide");
 			App.Tap(WelcomePage_GetStarted);
 
 			// Take a screenshot and add it to the test results
 			TakeScreenshot("After tapped");
 
 			Query important = q => q.All().Text("Important");
-			App.WaitForElement(important);
+			App.WaitForElement(important, timeoutMessage: "Timeout waiting for Important");
 			App.Tap(important);
 
 			// Take a screenshot and add it to the test results
 			TakeScreenshot("After important");
 
 			Query payBills = q => q.All().Text("Pay bills");
-			App.WaitForElement(payBills);
+			App.WaitForElement(payBills, timeoutMessage: "Timeout waiting for [Pay bills]");
 			App.Tap(payBills);
 
 			Query dueTime = q => q.All().Text("Due Wed, 13 April");
-			App.WaitForElement(dueTime);
+			App.WaitForElement(dueTime, timeoutMessage: "Timeout waiting for [Due Wed, 13 April]");
 
 			// Take a screenshot and add it to the test results
 			TakeScreenshot("After pay bill");
