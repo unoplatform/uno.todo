75e079d6947efddcebed20f831aa2027c419a3da
Carlín Yahuira Achahui <43356162+ClayAchahui@users.noreply.github.com>
2022-05-09 18:18:21 -0500

    Merge branch 'main' into dev/cya/add_duedate_task

--- src/ToDo/Presentation/Dialogs/ExpirationDateViewModel.cs
+++ src/ToDo/Presentation/Dialogs/ExpirationDateViewModel.cs
@@ -15,4 +15,17 @@ public partial class ExpirationDateViewModel
 	{
 		await _navigator.NavigateBackWithResultAsync(this, data: new DateTimeData { DateTime = DateTime.Today }, cancellation: ct);
 	}
+
+	public ICommand Tomorrow => Command.Async(DoTomorrow);
+	private async ValueTask DoTomorrow(CancellationToken ct)
+	{
+		await _navigator.NavigateBackWithResultAsync(this, data: new DateTimeData { DateTime = DateTime.Today.AddDays(1) }, cancellation: ct);
+	}
+
+	public ICommand NextWeek => Command.Async(DoNextWeek);
+	private async ValueTask DoNextWeek(CancellationToken ct)
+	{
+		await _navigator.NavigateBackWithResultAsync(this, data: new DateTimeData { DateTime = DateTime.Today.AddDays(7) }, cancellation: ct);
+	}
+
 }
