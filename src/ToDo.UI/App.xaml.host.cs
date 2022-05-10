a384531899ba9bcf48836d17447fcb0a00101243
Nick Randolph <nick@builttoroam.com>
2022-05-11 01:17:50 +1000

    feat: Addressing feedback

--- src/ToDo.UI/App.xaml.host.cs
+++ src/ToDo.UI/App.xaml.host.cs
@@ -47,6 +47,7 @@ public sealed partial class App : Application
 				// Load OAuth configuration
 				.UseConfiguration<Auth>()
 
+				// Enable app settings
 				.UseSettings<ToDoApp>()
 
 				// Register Json serializers (ISerializer and IStreamSerializer)
@@ -160,19 +161,4 @@ public sealed partial class App : Application
 		);
 	}
 }
-// TODO: Extract these to uno extensions
 
-public class AppTheme:IAppTheme
-{
-	private readonly Window _window;
-	public AppTheme(Window window)
-	{
-		_window = window;
-	}
-	public bool IsDark => SystemThemeHelper.IsRootInDarkMode(_window.Content.XamlRoot);
-
-	public void SetTheme(bool darkMode)
-	{
-		SystemThemeHelper.SetRootTheme(_window.Content.XamlRoot, darkMode);
-	}
-}
---
+++ src/ToDo.UI/AppTheme.cs
@@ -0,0 +1,18 @@
+﻿namespace ToDo;
+
+// TODO: Extract these to uno extensions
+// See https://github.com/unoplatform/uno.extensions/discussions/420
+public class AppTheme : IAppTheme
+{
+	private readonly Window _window;
+	public AppTheme(Window window)
+	{
+		_window = window;
+	}
+	public bool IsDark => SystemThemeHelper.IsRootInDarkMode(_window.Content.XamlRoot);
+
+	public void SetTheme(bool darkMode)
+	{
+		SystemThemeHelper.SetRootTheme(_window.Content.XamlRoot, darkMode);
+	}
+}
