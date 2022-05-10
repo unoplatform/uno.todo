830ddacd37e4a6e6b1b0531ec087142d7a3bcb74
David <github.site@torick.net>
2022-05-10 10:48:05 -0400

    feat: Update to new feed syntax

--- src/ToDo/Business/Models/ToDoTaskExtensions.cs
+++ src/ToDo/Business/Models/ToDoTaskExtensions.cs
@@ -2,12 +2,12 @@
 
 public static class ToDoTaskExtensions
 {
-	public static ToDoTask ToggleIsCompleted(this ToDoTask task)
+	public static ToDoTask WithToggledIsCompleted(this ToDoTask task)
 	{
 		return task with { Status = task.IsCompleted ? ToDoTask.TaskStatus.NotStarted : ToDoTask.TaskStatus.Completed };
 	}
 
-	public static ToDoTask ToggleImportance(this ToDoTask task)
+	public static ToDoTask WithToggledIsImportant(this ToDoTask task)
 	{
 		return task with { Importance = task.IsImportant ? ToDoTask.TaskImportance.Normal : ToDoTask.TaskImportance.Important };
 	}
--- src/ToDo/Presentation/TaskViewModel.cs
+++ src/ToDo/Presentation/TaskViewModel.cs
@@ -1,6 +1,5 @@
 ﻿namespace ToDo.Presentation;
 
-[ReactiveBindable]
 public partial class TaskViewModel
 {
 	private readonly INavigator _navigator;
@@ -9,27 +8,30 @@ public partial class TaskViewModel
 	private TaskViewModel(
 		INavigator navigator,
 		ITaskService svc,
-		IMessenger messenger,
 		ToDoTask entity)
 	{
 		_navigator = navigator;
 		_svc = svc;
 
-		Entity = State<ToDoTask>.Async(this, async _ => entity);
-		Entity.Execute(async (x, ct) =>
+		Entity = State.Value(this, () => entity);
+		Entity.Execute(async (task, ct) =>
 		{
-			if (x is not null)
+			if (task is not null)
 			{
-				await _svc.UpdateAsync(x, ct);
+				await _svc.UpdateAsync(task, ct);
 			}
 		});
 	}
 
 	public IState<ToDoTask> Entity { get; }
 
-	public ICommand Save => Command.Create(b => b.Given(Entity).Then(DoSave));
-	private async ValueTask DoSave(ToDoTask task, CancellationToken ct)
-		=> await _svc.UpdateAsync(task, ct);
+	public ICommand ToggleIsCompleted => Command.Create(b => b.Given(Entity).Then(DoToggleIsCompleted));
+	private async ValueTask DoToggleIsCompleted(ToDoTask task, CancellationToken ct)
+		=> await _svc.UpdateAsync(task.WithToggledIsCompleted(), ct);
+
+	public ICommand ToggleIsImportant => Command.Create(b => b.Given(Entity).Then(DoToggleIsImportant));
+	private async ValueTask DoToggleIsImportant(ToDoTask task, CancellationToken ct)
+		=> await _svc.UpdateAsync(task.WithToggledIsImportant(), ct);
 
 	public ICommand Delete => Command.Create(b => b.Given(Entity).Then(DoDelete));
 	private async ValueTask DoDelete(ToDoTask task, CancellationToken ct)
@@ -46,29 +48,5 @@ public partial class TaskViewModel
 			await _svc.DeleteAsync(task, ct);
 			await _navigator.NavigateBackAsync(this, cancellation: ct);
 		}
-	}
-
-	public ICommand ToggleIsCompleted => Command.Create(b => b.Given(Entity).Then(DoToggleIsCompleted));
-	private async ValueTask DoToggleIsCompleted(ToDoTask task, CancellationToken ct)
-	{
-		if (task.Status is null)
-		{
-			return;
-		}
-
-		var updatedTask = task with { Status = task.IsCompleted ? ToDoTask.TaskStatus.NotStarted : ToDoTask.TaskStatus.Completed };
-		await _svc.UpdateAsync(updatedTask, ct);
-	}
-
-	public ICommand ToggleIsImportant => Command.Create(b => b.Given(Entity).Then(DoToggleIsImportant));
-	private async ValueTask DoToggleIsImportant(ToDoTask task, CancellationToken ct)
-	{
-		if (task.Importance is null)
-		{
-			return;
-		}
-		var updatedTask = task with { Importance = task.IsImportant ? ToDoTask.TaskImportance.Normal : ToDoTask.TaskImportance.Important };
-
-		await _svc.UpdateAsync(updatedTask, ct);
 	}
 }
