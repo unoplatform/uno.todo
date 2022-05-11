30d54188ef31b85107236ce887728a7951e0887f
Agnès Zitte <agnes.zitte@nventive.com>
2022-05-11 02:23:00 -0400

    feat: Start to add Settings Flyout to fit mobile and desktop

---
+++ src/ToDo.UI/Views/Dialogs/SettingsFlyout.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace ToDo.Views.Dialogs;
+
+public sealed partial class SettingsFlyout : Flyout
+{
+	public SettingsFlyout()
+	{
+		this.InitializeComponent();
+	}
+
+	//TODO: Adjust logic to handle the Color Palette change and move it to the VM
+	// Related issue: https://github.com/unoplatform/uno.todo/issues/147
+	private void UpdateAppColorPalette(object sender, ChipItemEventArgs e)
+	{
+		//if (e.Item is Chip chip && GetX((string)chip.Tag) is { } something)
+		//{
+		//	// do stuff with something
+		//}
+
+		//object GetX(string tag) => tag switch
+		//{
+		//	"Purple" => "palette1.xaml",
+		//	"Blue" => "palette1.xaml",
+		//	"Yellow" => "palette1.xaml",
+
+		//	_ => throw new ArgumentOutOfRangeException(),
+		//}
+	}
+}
