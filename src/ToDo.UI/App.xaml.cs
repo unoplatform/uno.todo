a245d268e9488481dd1ba90635711c088ebd57d2
Nick Randolph <nick@builttoroam.com>
2022-10-31 15:48:45 +1100

    fix: Various updates

--- src/ToDo.UI/App.xaml.cs
+++ src/ToDo.UI/App.xaml.cs
@@ -14,10 +14,6 @@ public sealed partial class App : Application
 	public App()
 	{
 		this.InitializeComponent();
-
-#if HAS_UNO || NETFX_CORE
-		this.Suspending += OnSuspending;
-#endif
 	}
 
 	/// <summary>
@@ -61,30 +57,6 @@ public sealed partial class App : Application
 		SystemThemeHelper.SetRootTheme(_window.Content.XamlRoot, isDark);
 	}
 
-	/// <summary>
-	/// Invoked when Navigation to a certain page fails
-	/// </summary>
-	/// <param name="sender">The Frame which failed navigation</param>
-	/// <param name="e">Details about the navigation failure</param>
-	void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-	{
-		throw new InvalidOperationException($"Failed to load {e.SourcePageType.FullName}: {e.Exception}");
-	}
-
-	/// <summary>
-	/// Invoked when application execution is being suspended.  Application state is saved
-	/// without knowing whether the application will be terminated or resumed with the contents
-	/// of memory still intact.
-	/// </summary>
-	/// <param name="sender">The source of the suspend request.</param>
-	/// <param name="e">Details about the suspend request.</param>
-	private void OnSuspending(object sender, SuspendingEventArgs e)
-	{
-		var deferral = e.SuspendingOperation.GetDeferral();
-		// TODO: Save application state and stop any background activity
-		deferral.Complete();
-	}
-
 #pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
 	public void RouteUpdated(object? sender, RouteChangedEventArgs e)
 #pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
@@ -93,16 +65,6 @@ public sealed partial class App : Application
 		{
 			var rootRegion = e.Region.Root();
 			var route = rootRegion.GetRoute();
-
-
-#if !__WASM__ && !WINUI
-			CoreApplication.MainView?.DispatcherQueue.TryEnqueue(() =>
-			{
-				var appTitle = ApplicationView.GetForCurrentView();
-				appTitle.Title = "ToDo: " + (route + "").Replace("+", "/");
-			});
-#endif
-
 		}
 		catch (Exception ex)
 		{
